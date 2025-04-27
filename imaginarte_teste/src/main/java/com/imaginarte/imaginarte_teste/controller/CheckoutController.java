e748f993bf77d054d3a2715c28d9e39295db06df
Juliana Menezes <jmenezes799@gmail.com>
2025-04-27 16:59:00 -0300

    Implementando pagamento no checkout

--- imaginarte_teste/src/main/java/com/imaginarte/imaginarte_teste/controller/CheckoutController.java
+++ imaginarte_teste/src/main/java/com/imaginarte/imaginarte_teste/controller/CheckoutController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/checkout")
@@ -32,5 +33,34 @@ public class CheckoutController {
         session.setAttribute("enderecoSelecionado", enderecoId);
         return "redirect:/checkout/pagamento"; // Depois faz a tela de pagamento
     }
-}
 
+    @GetMapping("/pagamento")
+    public String mostrarPagamento() {
+        return "checkout/pagamento";  // Thymeleaf vai procurar no templates
+    }
+
+    @PostMapping("/checkout/pagamento")
+    public String processarPagamento(@RequestParam String formaPagamento,
+                                     @RequestParam(required = false) String numeroCartao,
+                                     @RequestParam(required = false) String codigoVerificador,
+                                     @RequestParam(required = false) String nomeCartao,
+                                     @RequestParam(required = false) String dataVencimento,
+                                     @RequestParam(required = false) int parcelas,
+                                     RedirectAttributes redirectAttributes) {
+
+        if (formaPagamento == null) {
+            redirectAttributes.addFlashAttribute("erro", "Por favor, selecione uma forma de pagamento.");
+            return "redirect:/checkout/pagamento";
+        }
+
+        if (formaPagamento.equals("cartao")) {
+            // Lógica para processar o pagamento com cartão
+            if (numeroCartao.isEmpty() || codigoVerificador.isEmpty() || nomeCartao.isEmpty() || dataVencimento.isEmpty() || parcelas <= 0) {
+                redirectAttributes.addFlashAttribute("erro", "Por favor, preencha todos os campos do cartão de crédito.");
+                return "redirect:/checkout/pagamento";
+            }
+        }
+        return "redirect:/checkout/validar";
+    }
+
+}
